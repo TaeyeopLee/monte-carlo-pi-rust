7189c795d265b2e3adadbac421752a36f82c55c3
TaeyeopLee <lee.taeyeop@hotmail.com>
2022-06-18 22:47:29 +0900

    complete monte-carlo-rust

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,26 @@ extern crate rand;
 use rand::Rng;
 
 fn main() {
+    let range: i32 = 10000000;
+    let mut cnt: i32 = 0;
     // generate 100 random number coordinates
-    for _cnt in 0..100 {
+    for _cnt in 0..range {
         let x: f64 = rand::thread_rng().gen();
         let y: f64 = rand::thread_rng().gen();
-        judge(x, y);
+        let judgeresult: bool = judge(x, y);
+        if judgeresult == true {
+            cnt += 1;
+        }
     }
+    println!("cnt {}", cnt);
 }
 
 
-fn judge(x: f64, y: f64) {
-    println!("x: {} y: {}", x, y);
-    // if (0.0 < x && x < 1.0) && (0.0 < y && y < 1.0) {
-    //     println!("true");
-    // }
+fn judge(x: f64, y: f64) -> bool {
+    let xplusy = x.powf(2.0) + y.powf(2.0);
+    let distance = xplusy.sqrt();
+    if distance >= 1.0 {
+        return false;
+    }
+    return true;
 }
